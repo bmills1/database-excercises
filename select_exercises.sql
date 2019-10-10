9cefaa5e584da92d35d5fbdb6181756c3346ec55
Bridget Mills <bridgetsmills@gmail.com>
2019-10-10 15:33:40 -0500

    added select file

---
+++ select_exercises.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+SELECT 'The name of all albums by Pink FloyD';
+SELECT * FROM albums WHERE artist = 'Pink Floyd';
+SELECT 'The year Sgt. Pepper''s Lonely Hearts Club Band was released';
+SELECT release_year FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+SELECT 'The genre for Nevermind';
+SELECT genre FROM albums WHERE name = 'Nevermind';
+SELECT 'Which albums were released in the 1990s';
+SELECT * FROM albums WHERE release_year BETWEEN 1899 AND 2001;
+SELECT 'Which albums had less than 20 million certified sales';
+SELECT * FROM albums WHERE sales <= 20;
+SELECT 'All the albums with a genre of Rock';
+SELECT * FROM albums WHERE genre = 'Rock';
