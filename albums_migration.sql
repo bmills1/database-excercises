6d9916318af6914452bf1e195abac1f3356a8cce
Bridget Mills <bridgetsmills@gmail.com>
2019-10-10 09:36:04 -0500

    updated albums table

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,12 +1,13 @@
 USE codeup_test_db;
 
+DROP TABLE IF EXISTS albums;
+
 CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist_first_name VARCHAR(50) DEFAULT 'NONE',
-    artist_last_name  VARCHAR(100),
-    record_name  VARCHAR(100),
-    release_date  DATE,
-    num_sales  FLOAT(10,2),
-    genre   VARCHAR(100),
-    PRIMARY KEY (id)
+    artist VARCHAR(250) NOT NULL,
+    name  VARCHAR(100) NOT NULL,
+    release_year  INT (4),
+    sales FLOAT(5,2) UNSIGNED,
+    genre VARCHAR(100),
+    PRIMARY KEY(id)
 );
