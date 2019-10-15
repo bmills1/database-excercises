37fcd48678df2adeaf6832d5c7c5541436b3825d
Bridget Mills <bridgetsmills@gmail.com>
2019-10-15 14:09:18 -0500

    refactored exercises

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -1,19 +1,15 @@
 USE employees;
 
 SELECT CONCAT(last_name, ', ', first_name) AS full_name
-FROM employees AS emp
-GROUP BY full_name, last_name
-ORDER BY last_name
-LIMIT 10;
-
-SELECT CONCAT(last_name, ', ', first_name)AS full_name, birth_date AS DOB
-FROM employees AS emp
-GROUP BY full_name, last_name, birth_date
-ORDER BY last_name
-LIMIT 10;
-
-SELECT CONCAT(emp_no, ' - ', last_name, ', ', first_name)AS full_name, birth_date AS DOB
 FROM employees
-GROUP BY emp_no, full_name, last_name, birth_date
-ORDER BY emp_no
+LIMIT 10;
+
+SELECT CONCAT(last_name, ', ', first_name) AS full_name,
+birth_date AS DOB
+FROM employees
+LIMIT 10;
+
+SELECT CONCAT(emp_no, ' - ', last_name, ', ', first_name) AS full_name,
+birth_date AS DOB
+FROM employees
 LIMIT 10;
--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -1,35 +1,38 @@
 USE employees;
+
 SELECT DISTINCT title FROM titles;
 
 SELECT last_name
 FROM employees
-WHERE last_name LIKE 'e%' and last_name LIKE '%e'
+WHERE last_name LIKE 'E%E'
 GROUP BY last_name;
 
-SELECT DISTINCT CONCAT(last_name,",", first_name) AS NAME
+SELECT last_name, first_name AS NAME
 FROM employees
-WHERE last_name LIKE 'e%' and last_name LIKE '%e'
+WHERE last_name LIKE 'E%E'
 GROUP BY last_name, first_name;
 
-SELECT DISTINCT last_name
+SELECT last_name
 FROM employees
-WHERE last_name LIKE '%q%' and last_name NOT LIKE '%qu%'
+WHERE last_name LIKE '%Q%' and last_name NOT LIKE '%QU%'
 GROUP BY last_name;
 
 SELECT DISTINCT last_name
 FROM employees
-WHERE last_name LIKE '%q%' and last_name NOT LIKE '%qu%'
+WHERE last_name LIKE '%Q%' and last_name NOT LIKE '%QU%'
 GROUP BY last_name;
 
 SELECT last_name, COUNT(*)
 FROM employees
-WHERE last_name LIKE '%q%' and last_name NOT LIKE '%qu%'
-GROUP BY last_name
-ORDER BY last_name;
+WHERE last_name LIKE '%Q%' and last_name NOT LIKE '%QU%'
+GROUP BY last_name;
 
 
-SELECT count(*),gender
+
+SELECT count(*) AS Total ,gender
 from employees
-where first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya'
+where first_name = 'Irena'
+   or first_name = 'Vidya'
+   or first_name = 'Maya'
 group by gender;
 
